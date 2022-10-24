28d4931da5faf5daff049dc1f931d650acdc99a3
Johan Lorenzo <jlorenzo@mozilla.com>
2022-10-24 11:28:23 +0200

    Attempt 2: Try to keep destination path the same

--- src/mozxchannel/android/importer.py
+++ src/mozxchannel/android/importer.py
@@ -41,6 +41,8 @@ def main():
 class Importer(object):
     def __init__(self, l10n_toml, dest):
         self.src_toml = l10n_toml
+        if dest == "mozilla-mobile/firefox-android" and "android-components" in l10n_toml:
+            dest = "mozilla-mobile/android-components"
         self.dest_toml = os.path.join(dest, 'l10n.toml')
         self.dest = dest
         self.src_config = self.dest_config = None
@@ -66,7 +68,7 @@ class Importer(object):
                         os.remove(file)
                 if not os.listdir(root):
                     os.removedirs(root)
-            
+
 
     def copy_toml(self):
         shutil.copy2(self.src_toml, self.dest_toml)
